6e3ec7830b86fbc970ab44634dbe4f3201bfd529
Laura B <laurabermudezg@gmail.com>
2019-04-09 23:52:24 -0700

    clean up code

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 
-
-
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyCoja7tz_bLX3YpuxzCn6uGZZ9kqeuRscE",
@@ -14,31 +12,26 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-// Initial Values
 var name = "";
 var destination = "";
 var firstTrain = "";
 var frequency = "";
 
-// Capture Button Click
+//on submit button click
 function addTrain() {
   event.preventDefault();
 
-  // Grabbed values from text boxes
   name = $("#trainName").val().trim();
   destination = $("#destination").val().trim();
   firstTrain = $("#firstTrainTime").val().trim();
   frequency = $("#frequency").val().trim();
 
-
-  // Code for handling the push
   database.ref('/trains/').push({
     name: name,
     destination: destination,
     firstTrain: firstTrain,
     frequency: frequency
   });
-
 }
 
 database.ref('/trains').on("child_added", function (trainEntry) {
@@ -50,30 +43,24 @@ database.ref('/trains').on("child_added", function (trainEntry) {
   });
 
   var train = trainEntry.val();
-
   var firstTrain = train.firstTrain;
   var frequency = train.frequency;
-
   var nextInfo = calculateNextTrain(firstTrain, frequency);
-
 
   row.append($('<td>').text(train.name));
   row.append($('<td>').text(train.destination));
   row.append($('<td>').text(train.frequency));
 
-
   var nextTrainTime = new $('<td>', {
     id: trainEntry.key + "-nextTrain",
     text: nextInfo[0]
   });
-
   row.append(nextTrainTime);
 
   var minutesLeft = new $('<td>', {
     id: trainEntry.key + "-inMins",
     text: nextInfo[1]
   });
-
   row.append(minutesLeft);
 
   $('#trainList').append(row);
@@ -81,6 +68,7 @@ database.ref('/trains').on("child_added", function (trainEntry) {
 });
 
 function updateTrainInfo() {
+
   database.ref('/trains').on("value", function (snapshot) {
     console.log(snapshot.val());
     var trains = snapshot.val();
@@ -93,26 +81,21 @@ function updateTrainInfo() {
       $('#' + trainId + '-inMins').text(nextInfo[1]);
 
     })
-
   });
-
-
 }
 
 function calculateNextTrain(firstTrain, frequency) {
-  var currentTime = moment().format('HH:mm');
+
   var timeSinceFirst = moment().diff(moment(firstTrain, "HH:mm"), "minutes");
-
   var minsRemainding = timeSinceFirst % frequency;
-
   var minsLeft = frequency - minsRemainding;
-
   var nextTrainTime = moment().add(minsLeft, 'minutes').format('HH:mm');
-
   return [nextTrainTime, minsLeft];
+
 }
 
 $(document).ready(function () {
 
   setInterval(updateTrainInfo, 60000);
+
 });
